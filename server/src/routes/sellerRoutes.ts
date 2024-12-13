ec08348ca7c10d6cdcf252f8720c708bc21fb524
iamakashrout <iamakashrout@gmail.com>
2024-12-13 20:30:32 +0530

    jwt verification

--- server/src/routes/sellerRoutes.ts
+++ server/src/routes/sellerRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import { addSeller, getSellersProducts, updateProduct, removeProduct } from "../controllers/sellerControllers";
+import { verifyToken } from "../middleware/auth";
 const router = express.Router();
 
-router.post("/addSeller", addSeller);
-router.get("/getSellerProducts/:sellerName", getSellersProducts);
-router.put("/updateProduct/:productId", updateProduct);
-router.delete("/removeProduct/:productId", removeProduct);
+router.post("/addSeller", verifyToken, addSeller);
+router.get("/getSellerProducts/:sellerName", verifyToken, getSellersProducts);
+router.put("/updateProduct/:productId", verifyToken, updateProduct);
+router.delete("/removeProduct/:productId", verifyToken, removeProduct);
 
 export default router;
