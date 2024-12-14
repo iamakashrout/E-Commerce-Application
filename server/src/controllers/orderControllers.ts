367d5b3f4ecfeb459cb7f6fa43ff76677f395da6
iamakashrout <iamakashrout@gmail.com>
2024-12-14 22:31:08 +0530

    order APIs updated

--- server/src/controllers/orderControllers.ts
+++ server/src/controllers/orderControllers.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import Order from "../models/orderSchema";
 import Product from "../models/productSchema";
+import User from "../models/userSchema";
 
+
+// place an order
 export const placeOrder = async (req: Request, res: Response): Promise<void> => {
     try {
         const { user, products, paymentMode, address, total } = req.body;
@@ -58,3 +61,71 @@ export const placeOrder = async (req: Request, res: Response): Promise<void> => 
         res.status(500).json({ success: false, error: "Failed to place order" });
     }
 };
+
+
+// view all orders of a user
+export const getAllOrders = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { user } = req.params;
+
+        const existingUser = await User.findOne({ email: user });
+        if (!existingUser) {
+            res.status(400).json({ success: false, error: "User does not exist" });
+            return;
+        }
+
+        const orders = await Order.find({ user });
+        res.status(200).json({ success: true, data: orders });
+    } catch (error) {
+        console.error("Error fetching orders:", error);
+        res.status(500).json({ success: false, error: "Failed to fetch all orders" });
+    }
+}
+
+// view details of a particular order
+export const getOrderDetails = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { orderId } = req.params;
+
+        const existingOrder = await Order.findOne({ orderId });
+        if (!existingOrder) {
+            res.status(400).json({ success: false, error: "Invalid order ID" });
+            return;
+        }
+
+        res.status(200).json({ success: true, data: existingOrder });
+    } catch (error) {
+        console.error("Error fetching order details:", error);
+        res.status(500).json({ success: false, error: "Failed to fetch order details" });
+    }
+}
+
+// cancel an order
+export const cancelOrder = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { orderId } = req.params;
+        const order = await Order.findOne({ orderId });
+        if (!order) {
+            res.status(400).json({ success: false, error: "Invalid order ID" });
+            return;
+        }
+        order.status = 'Cancelled';
+        const updatedOrder = await order.save();
+
+        res.status(200).json({ success: true, message: "Order cancelled successfully", data: updatedOrder });
+
+        // update stock of products
+        for (const product of order.products) {
+            const currProd = await Product.findOne({id: product.productId});
+            if(!currProd) {
+                res.status(400).json({ success: false, error: 'Product does not exist' });
+                return;
+            }
+            currProd.stock = currProd.stock + product.quantity;
+            await currProd.save();
+        }
+    } catch (error) {
+        console.error("Error cancelling order:", error);
+        res.status(500).json({ success: false, error: "Failed to cancel the order" });
+    }
+}
--- server/src/routes/orderRoutes.ts
+++ server/src/routes/orderRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth";
-import { placeOrder } from "../controllers/orderControllers";
+import { cancelOrder, getAllOrders, getOrderDetails, placeOrder } from "../controllers/orderControllers";
 
 const router = express.Router();
 
 router.post("/placeOrder", placeOrder);
+router.get("/getAllOrders/:user", getAllOrders);
+router.get("/getOrderDetails/:orderId", getOrderDetails);
+router.post("/cancelOrder/:orderId", cancelOrder);
 
 export default router;
