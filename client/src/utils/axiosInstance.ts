57e7e4a5401f3bbedbb44e5b084b11c8dd8840af
iamakashrout <iamakashrout@gmail.com>
2025-01-18 12:55:28 +0530

    backend deployed

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-commerce-application-1-i2ly.onrender.com/api",
   // timeout: 5000, // Timeout
   headers: {
     "Access-Control-Allow-Origin": "*",
