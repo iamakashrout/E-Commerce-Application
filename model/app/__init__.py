3ce9030e7917589d2ee7b258c12f38c1b1ecc911
iamakashrout <iamakashrout@gmail.com>
2025-01-19 19:00:58 +0530

    flask cors fix

--- model/app/__init__.py
+++ model/app/__init__.py
@@ -4,7 +4,7 @@ from .utils.db import db
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
     app.db = db # DB connection
 
