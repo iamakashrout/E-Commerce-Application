70bdef29297f54d08f8b0996c13c546dcc359b42
iamakashrout <iamakashrout@gmail.com>
2025-01-19 18:23:54 +0530

    frontend deloyment fixes

--- client/src/utils/flaskInstance.ts
+++ client/src/utils/flaskInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const flaskClient = axios.create({
-  baseURL: "http://localhost:8000", // Backend base URL
+  //baseURL: "http://localhost:8000", // Backend base URL
+  baseURL: "https://e-commerce-application-v6n6.onrender.com",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
--- client/src/utils/socketInstance.ts
+++ client/src/utils/socketInstance.ts
@@ -1,3 +1,4 @@
-const socketURL ="http://localhost:5000";
+//const socketURL ="http://localhost:5000";
+const socketURL = "https://e-commerce-application-1-i2ly.onrender.com";
 
 export default socketURL;
